461c921639765130f38fbf07c7ce26b875b80ce7
sowad223 <85169265+sowad223@users.noreply.github.com>
2025-01-18 08:48:17 +0600

    different logic

--- boy_or_girl.py
+++ boy_or_girl.py
@@ -1,10 +1,17 @@
 
+
 username = input()
 
-distinct_characters = set(username)
+char_count = {}
 
+for char in username:
+    if char not in char_count:
+        char_count[char] = 1
 
-if len(distinct_characters) % 2 == 0:
+distinct_count = len(char_count)
+
+if distinct_count % 2 == 0:
     print("CHAT WITH HER!")
 else:
     print("IGNORE HIM!")
+
